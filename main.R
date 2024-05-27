5b2ec917b16fd2f86cb6891679af387ed82d2a5e
Aleksandar <al.stojanovic0@gmail.com>
2024-05-27 14:35:38 +0200

    Data preprocessing

--- main.R
+++ main.R
@@ -1,10 +1,8 @@
-# 1 PRIPREMA PODATAKA
-
 # Konsultacije:
-#- ograniciti se samo na letnji period
+#- ograniciti se samo na letnji period +
 #- smanjiti broj koordinata, pogledati kako su prikupljani ti podaci, pa mozda
   #samo raditi predikcije za konkretne  meteo stanice
-#- razmotriti da li da se izbace sva merenja po mraku, sto je lako ako znamo da
+#- razmotriti da li da se izbace sva merenja po mraku, sto je lako ako znamo da +
   #se gleda samo letnji period
 #- ima smisla raditi predikciju ghi
 #- moze da se uradi klasifikacija preko tresholda nekog tipa jako zracenje i
@@ -19,58 +17,6 @@
 #- oni odnosi izmedju obelezja bukvalno trebaju svaki sa svakim da se uradi
 #- treba obratiti paznju na licencu i navesti na pocetku rada sve sto ta 
   #licenca zahteva, tipa naziv, autor, godina, naziv licence itd.
-
-# Ucitavanje datoteke
-
-#install.packages("BiocManager")
-#BiocManager::install("rhdf5")
-
-library(rhdf5)
-library(ggplot2)
-library(dplyr)
-library(lubridate)
-
-h5_file <- H5Fopen("./dataset/vietnam_2016.h5")
-
-
-# redovi: x y
-# Zakljucujemo da imamo 75361 redova zbog 75361 koordinate koje se posmatraju 
-# Kolona ima koliko i sati koliko je posmatranje trajalo 366 * 24 = 8784 
-
-
-air_temp <- h5read(h5_file, "/air_temperature") # celsius
-coordinates <- h5read(h5_file, "/coordinates") # ugao
-ghi <- h5read(h5_file, "/ghi") #kwh/m^2
-meta <- h5read(h5_file, "/meta") # elevation je u m 
-time_index <- h5read(h5_file, "/time_index") # datum
-wind_speed <- h5read(h5_file, "/wind_speed") # m/s
-
-# konvertujemo iz matricnog zapisa u tabelarni
-# Zbog velicine podataka uzimamo merenja vezana za svaku n-tu koordinatu
-n <- 10
-time_measures_per_coor = ncol(air_temp)
-coor_num = nrow(air_temp)
-
-selected_rows = seq(1, coor_num, by = n)
-coor_num <- length(selected_rows)
-
-id <- 1:(coor_num * time_measures_per_coor)
-
-latitude <- rep(coordinates[1, selected_rows], each = time_measures_per_coor)
-longitude <- rep(coordinates[2, selected_rows], each = time_measures_per_coor)
-elevation <- rep(meta[selected_rows,"elevation"], each = time_measures_per_coor)
-rm(coordinates)
-
-air_temp <- c(air_temp[selected_rows,])
-wind_speed <- c(wind_speed[selected_rows,])
-ghi <- c(ghi[selected_rows,])
-
-time <- rep(time_index, coor_num)
-
-
-df <- data.frame(id, time, latitude, longitude, elevation, air_temp, wind_speed, ghi)
-
-rm(h5_file, meta, id, time, latitude, longitude, elevation, air_temp, wind_speed, ghi)
 
 
 df <- df %>%
